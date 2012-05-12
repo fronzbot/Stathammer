3a1db01ae26c5252be140ae0912315bbf034d572
Kevin Fronczak <krazed1189@gmail.com>
2012-05-12 18:42:57 -0400

    Added distribution folder for releases.  Added setup.py which builds windows installer. Updated staticon.ico

---
+++ README.txt
@@ -0,0 +1,10 @@
+Stathammer is in no way afiliated with Games Workshop.  This
+tool uses basic statistical principles to calculate probabilities
+based on stats input by the user.  These stats are availaible by
+purchasing the approriate rulebooks and codices from http://games-workshop.com
+
+This program is currently in its alpha phase which means there's
+still a heck of a lot to go.  For any feature suggestions/bug reports
+please send an email to Kevin at kfronczak@gmail.com.  The program
+is still very malleable so if you have any features you'd like to see,
+this would be the most oppurtune time to ask!
---
+++ setup.py
@@ -0,0 +1,36 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+#build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+includefiles = ['README.txt', 'CHANGELOG.txt', 'staticon.ico']
+
+GUI2Exe_Target_1 = Executable(
+	# what to build
+	script = "stathammer.pyw",
+	initScript = None,
+	base = 'Win32GUI',
+	targetDir = r"build",
+	targetName = "Stathammer.exe",
+	compress = True,
+	copyDependentFiles = True,
+	appendScriptToExe = False,
+	appendScriptToLibrary = False,
+	icon = "staticon.ico"
+	)
+
+setup(
+        name = "Stathammer",
+        version = "0.03",
+        description = "Statistics Simulator for Warhammer 40k",
+        author = 'Kevin Fronczak',
+        author_email = 'kfronczak@gmail.com',
+        options = {'build_exe': {'include_files':includefiles}},
+        executables = [Executable("stathammer.pyw", base = base)])
